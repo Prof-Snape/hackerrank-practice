d652dc6ee41db6bdc5a39be8adc71f5bd2a1842b
Prof Snape <haris.hasnain@yahoo.in>
2022-06-29 19:02:52 +0530

    updates

--- leetcode_two_sum.py
+++ leetcode_two_sum.py
@@ -4,14 +4,33 @@
 
 # URL: https://leetcode.com/problems/two-sum/
 
-a = [int(x) for x in input().split()]
-sum_of_two = int(input())
+# If we are sure that elements of array is digits separated by whitespace then a shorter version of the code would be
+# to remove line 10 out from comment and put line 12 to line 26 in comment.
 
+# given_array = [int(x) for x in input().split()]
+
+given_array = input()
+given_array += " "
+given_array_with_int = []
+blank_string = ""
+
+for i in range(len(given_array)):
+    if given_array[i].isnumeric():
+        blank_string += "".join(given_array[i])
+    elif not given_array[i].isnumeric():
+        if len(blank_string) > 0:
+            blank_string = int(blank_string)
+            given_array_with_int.append(blank_string)
+            blank_string = ""
+
+given_array = given_array_with_int
+
+sum_of_two = int(input())
 result = []
 
-for i in range(len(a)):
-    for x in range(i + 1, len(a)):
-        if a[x] == (sum_of_two - a[i]):
+for i in range(len(given_array)):
+    for x in range(i + 1, len(given_array)):
+        if given_array[x] == (sum_of_two - given_array[i]):
             result.append(i)
             result.append(x)
             break
