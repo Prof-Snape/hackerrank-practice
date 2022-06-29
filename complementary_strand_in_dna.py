ed2466bf22798ce02bac657e619e9747ba8aaef1
Prof Snape <haris.hasnain@yahoo.in>
2022-06-29 09:09:08 +0530

    updates

---
+++ complementary_strand_in_dna.py
@@ -0,0 +1,36 @@
+# You are given the sequence of Nucleotides of one strand of DNA through a string S of length N. S contains the
+# character A, T, C, and G only.
+# Chef knows that:
+# A is complementary to T.
+# T is complementary to A.
+# C is complementary to G.
+# G is complementary to C.
+# Using the string S, determine the sequence of the complementary strand of the DNA.
+
+# URL = https://www.codechef.com/submit/DNASTRAND
+
+result = []
+
+test_case = int(input())
+
+for i in range(test_case):
+    length_of_dna = int(input())
+    dna = input()
+    complementary_dna = ""
+    for j in range(length_of_dna):
+        if dna[j] == "A":
+            complementary_dna += "T"
+        elif dna[j] == "T":
+            complementary_dna += "A"
+        elif dna[j] == "C":
+            complementary_dna += "G"
+        elif dna[j] == "G":
+            complementary_dna += "C"
+        else:
+            complementary_dna = "Invalid DNA!\n\tCase Sensitive."
+            break
+
+    result.append(complementary_dna)
+
+for x in result:
+    print(x)
--- roughpage.py
+++ roughpage.py
@@ -1,31 +1,5 @@
-result = []
-
-for x in range(int(input())):
-
-    numbers_of_element = int(input())
-    array_being_checked = [int(x) for x in input().split()]
-    array_being_checked.sort()
-
-    replace_with = array_being_checked[0]
-    occurrence = 1
-    previous_occurrence = 1
-    var = 0
-
-    for i in range(numbers_of_element - 1):
-        if array_being_checked[i] == array_being_checked[i + 1]:
-            occurrence += 1
-        elif occurrence > previous_occurrence:
-            replace_with = array_being_checked[i]
-            previous_occurrence = occurrence
-            occurrence = 1
-
-    if occurrence > previous_occurrence:
-        replace_with = array_being_checked[len(array_being_checked) - 1]
-
-    for i in range(len(array_being_checked)):
-        if array_being_checked[i] != replace_with:
-            var += 1
-    result.append(var)
-
-for i in result:
-    print(i)
+a = ["a", "o", "p"]
+b = ""
+for i in a:
+    b += "" + i
+print(b)
