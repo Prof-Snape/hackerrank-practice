8a19b398cdb0b6f2446927e7a08b5ae082789cfc
Prof Snape <haris.hasnain@yahoo.in>
2022-06-29 10:20:40 +0530

    updates

--- roughpage2.py
+++ roughpage2.py
@@ -1,17 +1,9 @@
-a = ["63 32 2"]
-b = []
-w = 0
+a = "010"
+b = "010101111010"
+occurrence = 0
 
-a1 = str(*a)
+for i in range(len(b)):
+    if b[i:].startswith(a):
+        occurrence += 1
 
-for i in range(len(a1)):
-    if a1[i] == " ":
-        w1 = int(a1[w:i])
-        b.append(w1)
-        w = i + 1
-
-w1 = int(a1[w:len(a1)])
-b.append(w1)
-
-b.sort()
-print(b)
+print(occurrence)
